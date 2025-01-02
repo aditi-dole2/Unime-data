27be4b372f9a3ef6c07bc41e3b3bd13d3c1650b2
aditi-dole2 <19aditidole@gmail.com>
2025-01-02 10:00:53 +0100

    Add files via upload

--- http/tcp_http_client.py
+++ http/tcp_http_client.py
@@ -1,1 +1,9 @@
-
+# # import socket
+# # def main():
+# #     host ='localhost'
+# #     port = 8080
+# #     hostname = (host,port)
+# #     with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as s:
+# #         s.connect(hostname)
+# #         print('socket status after connection%s '% (s))
+# #         print()
